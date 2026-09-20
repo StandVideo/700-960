48c127255920ae5872bb44b434c202b4d521f160
Stand <99733463+StandVideo@users.noreply.github.com>
2026-09-20 13:36:21 +0200

    Update goldhen_versions.js

--- files/js/goldhen_versions.js
+++ files/js/goldhen_versions.js
@@ -7,9 +7,14 @@ var goldhenVersions = [
     "disabled": true,
     "selected": true
   },
-    {
+  {
+    "value": "v2.4b18.11",
+    "text": "v2.4b18.11 - 20 Sep 2026 *Last Version*",
+    "fw": ["9.xx","8.xx","7.xx"]
+  },
+  {
     "value": "v2.4b18.10",
-    "text": "v2.4b18.10 - 29 Mai 2026 *Last Version*",
+    "text": "v2.4b18.10 - 29 Mai 2026",
     "fw": ["9.xx","8.xx","7.xx"]
   },
   {
